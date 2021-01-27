2f25248228a1a0403df014f0ab696b543d2ce4e9
noahlattari <zennoah1998@gmail.com>
2021-01-27 13:47:56 -0500

    initial files

---
+++ Assignments/Assignment 0/a0_script.m
@@ -0,0 +1,37 @@
+%% CPS843/CP8307 Assignment 0 - MATLAB Warm-up
+% Include this file in your submission. Do not modify it, other than to add 
+% your name and student number. The grader should be able to run this 
+% script to step through the entire assignment. 
+% - For multi-part questions, insert your own pauses, where appropriate.
+% - For written answer questions, print your answer in the MATLAB terminal.
+% Student Name: Noah Lattari
+% Student Number: 500760404
+
+%% Q1
+fprintf('Question 1 starting...\n');
+inputImages; %% write inputImages.m
+fprintf('Question 1 done! Press enter to continue...\n\n');
+pause;
+
+%% Q2
+fprintf('Question 2 starting...\n');
+colour;  %% write colour.m
+fprintf('Question 2 done! Press enter to continue...\n\n');
+pause;
+
+%% Q3
+fprintf('Question 3 starting...\n');
+replacement; %% write replacement.m
+fprintf('Question 3 done! Press enter to continue...\n\n');
+pause;
+
+%% Q4
+fprintf('Question 4 starting...\n');
+arithmetic; %% write arithmetic.m
+fprintf('Question 4 done! Press enter to continue...\n\n');
+pause;
+
+%% Q5
+fprintf('Question 5 starting...\n');
+noise;  %% write noise.m
+fprintf('Question 5 done!\n\n');
---
+++ Assignments/Assignment 0/inputImages.m
@@ -0,0 +1,1 @@
+fprintf('YO\n');

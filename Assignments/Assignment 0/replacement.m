a8d366f4729f4ce8fa31f29be3e3740ce926bc39
noahlattari <zennoah1998@gmail.com>
2021-01-30 13:20:28 -0500

    question 3

--- Assignments/Assignment 0/replacement.m
+++ Assignments/Assignment 0/replacement.m
@@ -1,6 +1,6 @@
 img2 = rgb2gray(imread('./images/splash.tiff'));
-figure, imshow(img2, [0,512]);
-figure, imshow(img_g, [0,512]);
+%figure, imshow(img2, [0,512]);
+%figure, imshow(img_g, [0,512]);
 
 box = img_g(206:306, 206:306);
 img2(206:306, 206:306) = box;

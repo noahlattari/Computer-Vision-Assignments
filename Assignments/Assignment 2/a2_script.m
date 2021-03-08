3c9665eb556c6f27e7502dc301bfe10b44e1deb0
noahlattari <zennoah1998@gmail.com>
2021-03-08 15:11:04 -0500

    finished qu 1

---
+++ Assignments/Assignment 2/a2_script.m
@@ -0,0 +1,9 @@
+ %% CPS843/CP8307 Assignment 2
+% Student Name: Noah Lattari
+% Student Number: 500760404
+
+% Q1
+fprintf('Question 1 starting...\n');
+leastfitting;
+fprintf('Question 1 done! Press enter to continue...\n\n');
+pause;
---
+++ Assignments/Assignment 2/leastfitting.m
@@ -0,0 +1,38 @@
+%~~ Kosta lecture code for reference ~~
+% x=[1:20]';
+% m = 2;
+% b = 5;
+% y=m*x+b;
+% y = y + randn(size(x));
+% A = [x ones(size(x))];
+% mb_estimate = A\y;
+
+%1.1
+alpha = 10;
+beta = 12;
+gamma = 15;
+
+x = rand([500 1]) - 0.5;
+y = rand([500 1]) - 0.5;
+z = alpha.*x + beta.*y + gamma;
+figure, scatter3(x,y,z);
+rotate3d;
+%noise adding
+z = z + (randn(size(x)) + randn(size(y)));
+
+%1.2
+%rewrite z = alphax+betay+gamma as Ax=b where x is the vector of unknowns
+
+temp = ones(size(x)); % to get gamma vector since gamma is not multiplied by anything, just set to ones
+A = [x,y,temp];
+
+%estimate x, our unknowns 
+x = A\z;
+
+%1.3
+alphaDiff = abs(x(1) - alpha);
+disp("The error in the alpha estimate is: " + alphaDiff);
+betaDiff = abs(x(2) - beta);
+disp("The error in the beta estimate is: " + betaDiff);
+gammaDiff = abs(x(3) - gamma);
+disp("The error in the gamma estimate is: " + gammaDiff);
